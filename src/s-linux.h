e3dac950a84e9507bb6470516dd1019385fc1d98
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2015-02-24 00:03:08 +0900

    Apply emacs-18.59-linux22x-elf-glibc21.diff, part 1.

--- src/s-linux.h
+++ src/s-linux.h
@@ -1,125 +1,260 @@
-/* Definitions file for GNU Emacs running on linux 0.96c pl2 and gcc 2.2.2
-   by Rick Sladkey <jrs@world.std.com>, your mileage may vary */
+/* Definitions file for GNU Emacs running on Linux
+   Copyright (C) 1985, 1986 Free Software Foundation, Inc.
 
-/*
-This file is part of Mule (MULtilingual Enhancement of GNU Emacs).
+This file is part of GNU Emacs.
 
-Mule is free software distributed in the form of patches to GNU Emacs.
-You can redistribute it and/or modify
+GNU Emacs is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 1, or (at your option)
 any later version.
 
-Mule is distributed in the hope that it will be useful,
+GNU Emacs is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU Emacs; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. */
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-/* 93.5.4   modified for Mule Ver.0.9.8
-   			by S.Komeda <komeda@ics.es.osaka-u.ac.jp>
-	Linux support updated. */
-/* 93.7.17  modified for Mule Ver.0.9.8
-			by Koaunghi Un <zraun01@hpserv.zdv.uni-tuebingen.de>
-	HAVE_SYSVIPC and LIB_X11_LIB is defined */
-/* 93.7.17  modified for Mule Ver.0.9.8
-   			by S.Komeda <komeda@ics.es.osaka-u.ac.jp>
-	ORDINARY_LINK is defined for libc.4.4 or greater. */
-/* 93.7.27  modified for Mule Ver.0.9.8
-   			by S.Komeda <komeda@ics.es.osaka-u.ac.jp>
-	Modified to avoid wchar_t problem. */
-/* 94.3.10  modified for Mule Ver.1.1 by F.Ueno <ueno@al.t.u-tokyo.ac.jp>
-	Updated. */
+/* $Id: s-linux.h,v 1.1.2.3 1999/11/05 08:17:23 friedman Exp $ */
+/* This config.h written for Linux 2.0 ELF, gcc 2.7, libc 5.2, and X11R6 */
 
-#include "s-usg5-2.h"		/* as close as anything */
+/*
+ *	Define symbols to identify the version of Unix this is.
+ *	Define all the symbols that apply correctly.
+ */
 
-#ifdef SYSTEM_TYPE
-#undef SYSTEM_TYPE
-#endif
+#define USG				/* System III, System V, etc */
+#define USG5
+
+/* SYSTEM_TYPE should indicate the kind of system you are using.
+ It sets the Lisp variable system-type.  */
+
 #define SYSTEM_TYPE "linux"
 
-/* overrides for linux versus s-usg5-2.h */
+/* nomultiplejobs should be defined if your system's shell
+ does not have "job control" (the ability to stop a program,
+ run some other program, then continue the first one).  */
 
-#undef TERMINFO			/* not really SYSV */
-#undef COFF			/* not really SYSV */
-#undef NOMULTIPLEJOBS		/* not even used ... */
-#undef NONSYSTEM_DIR_LIBRARY	/* use our dirent library for VFS */
-#undef static			/* static is OK for with gcc */
-#undef sigsetmask		/* linux has sigsetmask */
-#undef _setjmp			/* we must use macro in setjmp.h */
+/* #define NOMULTIPLEJOBS */
 
-/* just a few small changes for linux ... */
+/* This requires SIGIO to work properly; Linux 2.0 has it. */
+#define INTERRUPT_INPUT
+
+/* Letter to use in finding device name of first pty,
+  if system supports pty's.  'p' means it is /dev/ptyp0  */
+
+#define FIRST_PTY_LETTER 'p'
+
+/*
+ *	Define HAVE_TERMIO if the system provides sysV-style ioctls
+ *	for terminal control.
+ */
+
+#define HAVE_TERMIO
+
+/*
+ *	Define HAVE_TIMEVAL if the system supports the BSD style clock values.
+ *	Look in <sys/time.h> for a timeval structure.
+ */
+
+#define HAVE_TIMEVAL
+
+/*
+ *	Define HAVE_SELECT if the system supports the `select' system call.
+ */
+
+#define HAVE_SELECT
+
+/*
+ *	Define HAVE_PTYS if the system supports pty devices.
+ */
+
+#define HAVE_PTYS
+
+/* Define HAVE_SOCKETS if system supports 4.2-compatible sockets.  */
+
+#define HAVE_SOCKETS
+
+/*
+ *	Define NONSYSTEM_DIR_LIBRARY to make Emacs emulate
+ *      The 4.2 opendir, etc., library functions.
+ */
+
+/* #define NONSYSTEM_DIR_LIBRARY */
+
+/* Define this symbol if your system has the functions bcopy, etc. */
+
+#define BSTRING
+
+/* subprocesses should be defined if you want to
+ have code for asynchronous subprocesses
+ (as used in M-x compile and M-x shell).
+ This is supposed to work now on system V release 2.  */
+
+#define subprocesses
+
+/* If your system uses COFF (Common Object File Format) then define the
+   preprocessor symbol "COFF". */
+
+/* #define COFF */
+
+/* define MAIL_USE_FLOCK if the mailer uses flock
+   to interlock access to /usr/spool/mail/$USER.
+   The alternative is that a lock file named
+   /usr/spool/mail/$USER.lock.  */
+
+/* #define MAIL_USE_FLOCK */
+
+/* Define CLASH_DETECTION if you want lock files to be written
+   so that Emacs can tell instantly when you try to modify
+   a file that someone else has modified in his Emacs.  */
+
+#define CLASH_DETECTION
+
+/* Define SHORTNAMES if the C compiler can distinguish only
+   short names.  It means that the stuff in ../shortnames
+   must be run to convert the long names to short ones.  */
+
+/* #define SHORTNAMES */
+
+/* extend max filesize to 32 Meg */
+#define VALBITS 26
+#define GCTYPEBITS 5
+
+
+/* Special hacks needed to make Emacs run on this system.  */
+
+/* On USG systems the system calls are interruptable by signals
+ that the user program has elected to catch.  Thus the system call
+ must be retried in these cases.  To handle this without massive
+ changes in the source code, we remap the standard system call names
+ to names for our own functions in sysdep.c that do the system call
+ with retries. */
+
+#define open sys_open
+#define close sys_close
+#define read sys_read
+#define write sys_write
+
+#define INTERRUPTABLE_OPEN
+#define INTERRUPTABLE_CLOSE
+#define INTERRUPTABLE_IO
 
 /* let's see, what have we got here */
 
 #define HAVE_TCATTR		/* fixes ^z problems */
 #define HAVE_SETSID		/* fixes shell problems */
 #define HAVE_DUP2		/* is builtin */
-#define HAVE_TIMEVAL		/* is builtin */
 #define HAVE_GETTIMEOFDAY	/* is builtin */
 #define HAVE_RENAME		/* is builtin */
 #define HAVE_RANDOM		/* is builtin */
-#define HAVE_SELECT		/* works */
-#define HAVE_PTYS		/* works */
 #define HAVE_CLOSEDIR		/* we have a closedir */
 #define HAVE_GETPAGESIZE	/* we now have getpagesize (0.96) */
 #define HAVE_VFORK		/* we now have vfork (0.96) */
 #define HAVE_SYS_SIGLIST	/* we have a (non-standard) sys_siglist */
+#define SYS_SIGLIST_DECLARED
 #define HAVE_GETWD		/* cure conflict with getcwd? */
-#define	HAVE_SOCKETS		/* we have socket */
-#define	NO_SOCK_SIGIO 		/* linux doesn't have SIGIO */
 
-#define BSTRING			/* we now have bcopy, etc. (0.96) */
-#define USE_UTIME		/* don't have utimes */
 #define NO_SIOCTL_H		/* don't have sioctl.h */
 #define SYSV_SYSTEM_DIR		/* use dirent.h */
 #define USG_SYS_TIME		/* use sys/time.h, not time.h */
-#define HAVE_SYSVIPC		/* 93.7.17 by K.Un - 0.99.10 has working IPC */
 
-#define INTERRUPTABLE_CLOSE	/* no harm if not true */
-#define close sys_close
+#define POSIX			/* affects only getpagesize.h */
+#define POSIX_SIGNALS		/* uses sigaction from sys_signal */
 
-#define C_COMPILER gcc
-#define C_DEBUG_SWITCH -O6 -fwritable-strings -fomit-frame-pointer -D__const=
-#define C_OPTIMIZE_SWITCH -O6 -fwritable-strings -fomit-frame-pointer -D__const=
-#define OLDXMENU_OPTIONS CFLAGS=-O6 EXTRA=insque.o /* doesn't work anyway */
-#define LIB_X11_LIB -L/usr/X386/lib -lX11 /* 93.7.17 by K.Un */
-/* 93.7.17 by S.Komeda */
-#define ORDINARY_LINK   /* need if you use libc.4.4 or greater */
+#ifdef HAVE_PTMX
 
+/* This change means that we don't loop through allocate_pty too many
+   times in the (rare) event of a failure. */
+
+#undef FIRST_PTY_LETTER
+#define FIRST_PTY_LETTER 'z'
+
+/* This sets the name of the master side of the PTY. */
+
+#define PTY_NAME_SPRINTF strcpy (pty_name, "/dev/ptmx");
+
+/* This is the same definition as in usg5-4.h, but with sigblock/sigunblock
+   rather than sighold/sigrelse, which appear to be BSD4.1 specific and won't
+   work if POSIX_SIGNALS is defined.  It may also be appropriate for SVR4.x
+   (x<2) but I'm not sure.   fnf@cygnus.com */
+/* This sets the name of the slave side of the PTY.  On SysVr4,
+   grantpt(3) forks a subprocess, so keep sigchld_handler() from
+   intercepting that death.  If any child but grantpt's should die
+   within, it should be caught after sigrelse(2). */
+
+#undef PTY_TTY_NAME_SPRINTF
+#define PTY_TTY_NAME_SPRINTF			\
+  {						\
+    char *ptsname (), *ptyname;			\
+						\
+    sigblock (sigmask (SIGCLD));		\
+    if (grantpt (fd) == -1)			\
+      { close (fd); return -1; }		\
+    sigunblock (sigmask (SIGCLD));		\
+    if (unlockpt (fd) == -1)			\
+      { close (fd); return -1; }		\
+    if (!(ptyname = ptsname (fd)))		\
+      { close (fd); return -1; }		\
+    strncpy (pty_name, ptyname, sizeof (pty_name)); \
+    pty_name[sizeof (pty_name) - 1] = 0;	\
+  }
+
+#endif /* HAVE_PTMX */
+
+/* Unless this is defined, XFlush crashes emacs because of fd duping
+   performed in x11term.c (x_init_1). */
+#define SYSV_STREAMS
+
+/* note: system malloc does not work with shared libs
+   This was reported with earlier versions of linux (libc 4).
+   Still true?
+ */
 #if 0				/* choose for yourself */
 #define SYSTEM_MALLOC		/* produces smaller binary */
-#else
-#define ULIMIT_BREAK_VALUE (32*1024*1024) /* ulimit not implemented */
-#endif
-
-#undef rcheck			/* for debugging builtin malloc */
-
-#ifdef rcheck
-#define botch(msg)	(printf("%s", (msg)), abort())
 #endif
 
 /* misc. kludges for linux */
-
-#define __const 		/* avoids type mismatch errors */
-
+#if !(defined (__GLIBC__) && (__GLIBC__ >= 2))
 #define MAXNAMLEN NAME_MAX	/* missing SYSV-ism */
+#endif
 
-/* #define SIGBUS SIGSEGV		/* rename to harmless work-alike */
-#define SIGSYS SIGSEGV		/* rename to harmless work-alike */
-
+#define SIGSYS SIGUNUSED	/* rename to harmless work-alike */
 #define VSWTCH VSWTC		/* mis-spelling in termios.h? */
-#define CDEL '\0'		/* missing termio-ism */
 
 /* we have non-standard standard I/O (iostream) ... */
+#ifdef emacs
+#include <stdio.h>  /* Get the definition of _IO_STDIO_H.  */
+#if defined(_IO_STDIO_H) || defined(_STDIO_USES_IOSTREAM)
+/* new C libio names */
+#define PENDING_OUTPUT_COUNT(FILE) \
+  ((FILE)->_IO_write_ptr - (FILE)->_IO_write_base)
+#else /* !_IO_STDIO_H */
+/* old C++ iostream names */
+#define PENDING_OUTPUT_COUNT(FILE) \
+  ((FILE)->_pptr - (FILE)->_pbase)
+#endif /* !_IO_STDIO_H */
+#endif /* emacs */
 
-/* #define PENDING_OUTPUT_COUNT(FILE) ((FILE)->_pptr - (FILE)->_pbase) */
-#define PENDING_OUTPUT_COUNT(FILE) ((FILE)->_IO_write_ptr - (FILE)->_IO_write_base)
+/* This is for debugging; otherwise, gdb cannot access numerous structure
+   members like XXdisplay->fd, etc. */
+#define XLIB_ILLEGAL_ACCESS
 
-/* defines for linux in preparation for m-intel386.h */
+#ifdef __ELF__
+#define UNEXEC unexelf.o
+#define UNEXEC_USE_MAP_PRIVATE
+#define DATA_SEG_BITS 0x08000000
+#endif
 
-#define DONT_DEFINE_SIGNAL	/* live with the warnings */
+#define C_COMPILER gcc
+#define C_DEBUG_SWITCH -g
+#define C_OPTIMIZE_SWITCH -O3 -malign-loops=2 -malign-jumps=2 -malign-functions=2
+#define OLDXMENU_OPTIONS CFLAGS=-O2 EXTRA=insque.o
+#define START_FILES pre-crt0.o /usr/lib/crt1.o /usr/lib/crti.o
+#define LIBS_DEBUG		/* override in config.h to include -lg */
+#define LIBS_TERMCAP -lncurses
+#define LIBS_SYSTEM -lc /usr/lib/crtn.o
 
+/* s-linux.h ends here */
