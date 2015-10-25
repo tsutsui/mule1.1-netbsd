85248c0fa91feef8b00f469dee87c3c1559d9156
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2015-10-25 12:08:56 +0900

    xmalloc() and xrealloc() should take size_t as malloc().

--- src/alloc.c
+++ src/alloc.c
@@ -148,7 +148,7 @@ memory_full ()
 
 void *
 xmalloc (size)
-     int size;
+     size_t size;
 {
   register void *val;
   /* Avoid failure if malloc (0) returns 0.  */
@@ -162,7 +162,7 @@ xmalloc (size)
 void *
 xrealloc (block, size)
      void *block;
-     int size;
+     size_t size;
 {
   register void *val;
   /* Avoid failure if malloc (0) returns 0.  */
